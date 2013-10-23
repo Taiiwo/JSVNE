30ebc6338d1f191232e371108c78572944eecfad
Taiiwo Llort <Tylerzacc@gmail.com>
2013-10-23 07:17:58 +0100

    Add story.js and remove story.json

---
+++ story.js
@@ -0,0 +1,34 @@
+var story = [
+		[//act one
+			{//scene one
+				'background':'http://images.4chan.org/b/src/1382491220184.jpg',
+				'text':'This is super scary.',
+				'music':'http://k002.kiwi6.com/hotlink/60ly26xb0b/p',
+				'type':'story'
+			},
+			{//scene two
+                                'background':'http://images.4chan.org/b/src/1382491649162.jpg',
+                                'text':'Super duper scary..',
+                                'music':'',
+                                'type':'story'
+                        }
+
+		],
+                [//act two
+                        {//scene one
+                                'background':'scary.jpg',
+                                'text':'This is super scary.',
+                                'music':'scary.mp3',
+                                'type':'story'
+                        },
+                        {//scene two
+                                'background':'scary1.jpg',
+                                'text':'What will you do?.',
+                                'music':'scary.mp3',
+                                'type':'decision',
+				'options':[['do this',2],['do that',3],['do something else',4]]
+                        } 
+
+                ]
+
+	];
