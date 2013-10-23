24704f91b3a60c3f8d99e9b2908b261df3f4f94e
Taiiwo Llort <Tylerzacc@gmail.com>
2013-10-23 07:13:08 +0100

    update

--- engine.js
+++ engine.js
@@ -1,60 +1,28 @@
-var story = [
-		[//act one
-			{//scene one
-				'background':'flooded-hall.jpg',
-				'text':'This is super scary.',
-				'music':'squid',
-				'type':'story'
-			},
-			{//scene two
-                                'background':'scary1.jpg',
-                                'text':'Super duper scary..',
-                                'music':'scary.mp3',
-                                'type':'story'
-                        }
-
-		],
-                [//act two
-                        {//scene one
-                                'background':'scary.jpg',
-                                'text':'This is super scary.',
-                                'music':'scary.mp3',
-                                'type':'story'
-                        },
-                        {//scene two
-                                'background':'scary1.jpg',
-                                'text':'What will you do?.',
-                                'music':'scary.mp3',
-                                'type':'decision',
-				'options':['do this','do that','do something else']
-                        } 
-
-                ]
-
-	]
-$( "#game" ).click(function() {
-	next();
-});
 function setbg(bgname) {
-	var path = "./images/backgrounds/" + bgname;
+	var path = bgname;
 	document.getElementById('game').style.backgroundImage = "url(" + path + ")";
 }
 function settext(text){
 	document.getElementById("speech").innerHTML = '<p>' + text + "</p>";
 }
 function setmusic(trackname){
-	var path = "./sounds/music/" + trackname;
-	document.getElementById('music').innerHTML = '\
+	var path = trackname;
+	var dryrun = '\
 <audio hidden autoplay loop>\
-	<source src="' + path + '.ogg" type="audio/ogg" />\
-	<source src="' + path + '.mp3" type="audio/mpeg" />\
-	<source src="' + path + '.wav" type="audio/wav" />\
+        <source src="' + path + '.ogg" type="audio/ogg" />\
+        <source src="' + path + '.mp3" type="audio/mpeg" />\
+        <source src="' + path + '.wav" type="audio/wav" />\
 </audio>';
+	if (document.getElementById('music').innerHTML != '') {
+		document.getElementById('music').innerHTML = dryrun;
+	}
 }
 function decide(options){
+	var option = "";
 	for (var b = 0; b < options.length; b++) {
-		document.getElementById(
+		option += '<p id="option' + b + '" class="options">' + options[b][0] + '</p>';
 	}
+	document.getElementById('options').innerHTML = option;
 }
 var i = 0;
 var a = 0;
@@ -67,8 +35,7 @@ function next(){
 	settext(story[i][a]["text"]);
 	setmusic(story[i][a]["music"]);
 	if (story[i][a]["type"] == "decision") {
-		for (var b = 0; b < story[i][a]["options"].length; b++) {
-			
-		}
+		decide(story[i][a]["options"]);
+	}
 	a++;
 }
